4d34f24f0e2c9a53493ddc41d2e33f9cfb0e3ad4
MatheusHBMelo <matheushbmelo@gmail.com>
2021-12-05 11:23:53 -0300

    Variaveis

---
+++ Variáveis, tipos de dados e operadores matemáticos/Variaveis/Variaveis.java
@@ -0,0 +1,27 @@
+public class Variaveis{
+    public static void main(String[] args){
+        // Numericas
+        byte oneByte = -128;
+        byte twoByte = 127;
+
+        short oneShort = -32768;
+        short twoShort = 32767;
+
+        int oneInt = -2147483648;
+        int twoInt = 2147483647;
+
+        long oneLong = -922337204;
+        long twoLong = 922337202;
+
+        float oneFloat = 5.5f;
+
+        double oneDouble = 5.5;
+
+        // Caractere
+        String oneString = "Matheus";
+        char oneChar = 'm';
+
+        // Booleanas
+        boolean oneBoolean = true;
+    }
+}
